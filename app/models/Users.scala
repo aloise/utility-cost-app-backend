de52ae1608a239fee07d8db3cdc1f0b49d87d70c
aloise <aloise@aloise.name>
2016-05-23 18:38:02 +0300

    [+] db - initial

---
+++ app/models/Users.scala
@@ -0,0 +1,37 @@
+package models
+
+import scala.concurrent.Future
+import javax.inject.Inject
+
+import play.api.db.slick.DatabaseConfigProvider
+import play.api.db.slick.HasDatabaseConfigProvider
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import slick.driver.JdbcProfile
+
+/**
+  * User: aloise
+  * Date: 23.05.16
+  * Time: 18:32
+  */
+
+case class User( id:Int, name:String, email:String, password:String )
+
+class Users @Inject()(protected val dbConfigProvider: DatabaseConfigProvider) extends HasDatabaseConfigProvider[JdbcProfile] {
+
+  import driver.api._
+
+  private val Cats = TableQuery[CatsTable]
+
+  def all(): Future[Seq[User]] = db.run(Cats.result)
+
+  def insert(cat: User): Future[Unit] = db.run(Cats += cat).map { _ => () }
+
+  private class CatsTable(tag: Tag) extends Table[User](tag, "CAT") {
+
+    def name = column[String]("NAME", O.PrimaryKey)
+    def color = column[String]("COLOR")
+
+    def * = (name, color) <> (User.tupled, User.unapply _)
+  }
+
+}
--- build.sbt
+++ build.sbt
@@ -2,12 +2,21 @@ name := "UtilityCostAppBackend"
 
 version := "1.0"
 
-lazy val `utilitycostappbackend` = (project in file(".")).enablePlugins(PlayScala)
+lazy val root = (project in file(".")).enablePlugins(PlayScala)
 
-scalaVersion := "2.11.7"
-
-libraryDependencies ++= Seq( jdbc , cache , ws   , specs2 % Test )
+scalaVersion := "2.11.8"
 
 unmanagedResourceDirectories in Test <+=  baseDirectory ( _ /"target/web/public/test" )  
 
-resolvers += "scalaz-bintray" at "https://dl.bintray.com/scalaz/releases"  
+resolvers += "scalaz-bintray" at "https://dl.bintray.com/scalaz/releases"
+
+
+libraryDependencies ++= Seq(
+  jdbc ,
+  cache ,
+  ws   ,
+  "com.typesafe.play" %% "play-slick" % "2.0.0",
+  "com.typesafe.play" %% "play-slick-evolutions" % "2.0.0",
+  "com.h2database" % "h2" % "1.4.191",
+  specs2 % Test
+)
