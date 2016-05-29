4cbaeeec4c738303fb1fa2506d96a16e9921c4a6
Dmitry Svetlakov <mail@ae0n.name>
2016-05-29 21:33:59 +0500

    [*] controllers - added update and create actions to places

--- app/controllers/api/Places.scala
+++ app/controllers/api/Places.scala
@@ -2,10 +2,13 @@ package controllers.api
 
 import javax.inject.Inject
 
-import models.PlacesQuery
+import models.{PlacesQuery, UserRole, UsersPlace, UsersPlacesQuery}
 import models.base.DBAccessProvider
+import models.helpers.ModelToJsonHelper
+import play.api.libs.json.Json
 import slick.driver.H2Driver.api._
 import slick.driver.JdbcProfile
+
 import scala.concurrent.ExecutionContext
 
 /**
@@ -13,12 +16,36 @@ import scala.concurrent.ExecutionContext
   * Date: 27.05.16
   * Time: 22:56
   */
-class Places @Inject() ( implicit ec:ExecutionContext, db: DBAccessProvider ) extends ApiController(ec, db) {
-
+class Places @Inject()(implicit ec: ExecutionContext, db: DBAccessProvider) extends ApiController(ec, db) {
+import ModelToJsonHelper.placesToJson
   def list = apiWithAuth { user => r =>
-    db.run( PlacesQuery.forUser(user.id.getOrElse(0)).result ).map{ items =>
+    db.run(PlacesQuery.forUser(user.id.getOrElse(0)).result).map { items =>
       jsonStatusOk
     }
+  }
+
+  def create = apiWithParser(ModelToJsonHelper.placesToJson) { user => place =>
+    db.run(PlacesQuery.insert(place).flatMap { newId =>
+      PlacesQuery.findById(newId)
+    }).map { place =>
+      jsonStatusOk(Json.obj("place" -> Json.toJson(place)))
+    }
+  }
+
+  def update = apiWithParser(ModelToJsonHelper.placesToJson) { user => place =>
+
+    place.id match {
+      case Some(id) =>
+        db.run(
+          UsersPlacesQuery.findUserPlace(user.id.getOrElse(-1), id, UserRole.Admin)
+            .flatMap { userPlace =>
+              PlacesQuery.update(id, place).flatMap(PlacesQuery.findById).map { updatedPlace =>
+                jsonStatusOk(Json.obj("place" -> Json.toJson(updatedPlace)))
+              }
+            })
+      case None => recoverJsonErrorsFuture("no_id")
+    }
+
 
   }
 
